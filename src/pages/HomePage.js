c047ef44808beb68f56251f39ca943756c1bcf09
Malitha Sayuranga <lghmalith44@gmail.com>
2024-09-01 01:36:47 +0530

    Initial commit of ABC Restaurant frontend  pull

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+// src/pages/HomePage.js
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to ABC Restaurant</h1>
+            <p>Explore our menu, make reservations, and more!</p>
+        </div>
+    );
+}
+
+export default HomePage;
